435192189ac6bbdf6d439742cb84aea71d9a0fbe
Izcar J. Muñoz Torrez <izcarmt95@gmail.com>
2024-03-31 17:54:55 -0600

    feat: add html for swagger ui

--- tests/test_project/test_app/views.py
+++ tests/test_project/test_app/views.py
@@ -1,9 +1,18 @@
-from django.http import HttpResponse
-from django.views import View
-from djestful import main
+from django.http import HttpResponse, JsonResponse
+from djestful.openapi.docs import get_swagger_ui_html
+from djestful.views import APIView
 
 
-class TestView(View):
-    def get(self, request, *args, **kwargs):
-        main()
+class TestView(APIView):
+    def _test(i: int):
+        return i + 1
+
+    def get(self, request: int, *args, **kwargs) -> HttpResponse:
+        print(self._test.__annotations__)
+        return get_swagger_ui_html(
+            title='Test API',
+            openapi_url='http://localhost:8000/openapi.json',
+        )
+
+    def post(self, request, *args, **kwargs) -> HttpResponse:
         return HttpResponse('Hello, World!')

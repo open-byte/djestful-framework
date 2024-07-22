8316ac03550cc0cd3cee888d2b1cb88191136efb
Izcar J. Muñoz Torrez <izcarmt95@gmail.com>
2024-07-21 21:05:11 -0600

    fix: remove duplicated code

--- djestful/urls.py
+++ djestful/urls.py
@@ -13,28 +13,10 @@ def _describe_pattern(pattern: URLPattern | URLResolver) -> str:
     return str(pattern.pattern)
 
 
-def get_urls_view_mapping() -> list[tuple[str, Callable[..., Any]]]:
-    resolver = get_resolver()
+def _get_urls_view_mapping(prefix: str, url_pattern: list[URLResolver | URLPattern]) -> Any:
     url_view_mapping: list[tuple[str, Callable[..., Any]]] = []
-
-    def _get_urls_view_mapping(prefix: str, url_pattern: list[URLResolver]) -> Any:
-        for item in url_pattern:
-            _prefix = prefix + _describe_pattern(item)
-            if (
-                isinstance(item, URLPattern)
-                and hasattr(item.callback, 'view_class')
-                and issubclass(item.callback.view_class, APIView)
-            ):
-                url_view_mapping.append((_describe_pattern(item), item.callback))
-
-            elif isinstance(item, URLResolver) and hasattr(item, 'url_patterns'):
-                if hasattr(item, 'url_patterns'):
-                    _get_urls_view_mapping(_prefix, item.url_patterns)  # type: ignore[arg-type]
-
-                if hasattr(item, 'callback') and item.callback:
-                    url_view_mapping.append((_prefix, item.callback))
-
-    for item in resolver.url_patterns:
+    for item in url_pattern:
+        _prefix = prefix + _describe_pattern(item)
         if (
             isinstance(item, URLPattern)
             and hasattr(item.callback, 'view_class')
@@ -42,14 +24,19 @@ def get_urls_view_mapping() -> list[tuple[str, Callable[..., Any]]]:
         ):
             url_view_mapping.append((_describe_pattern(item), item.callback))
 
-        elif isinstance(item, URLPattern) and hasattr(item.callback, DJESTFUL_ATTRS):
-            url_view_mapping.append((_describe_pattern(item), item.callback))
-
-        elif isinstance(item, URLResolver):
+        elif isinstance(item, URLResolver) and hasattr(item, 'url_patterns'):
             if hasattr(item, 'url_patterns'):
-                _get_urls_view_mapping(_describe_pattern(item), item.url_patterns)  # type: ignore[arg-type]
+                url_view_mapping.extend(_get_urls_view_mapping(_prefix, item.url_patterns))
 
             if hasattr(item, 'callback') and item.callback:
-                url_view_mapping.append((_describe_pattern(item), item.callback))
+                url_view_mapping.append((_prefix, item.callback))
+
+    return url_view_mapping
+
+
+def get_urls_view_mapping() -> list[tuple[str, Callable[..., Any]]]:
+    url_pattern: list[URLResolver | URLPattern] = get_resolver().url_patterns
+
+    url_view_mapping: list[tuple[str, Callable[..., Any]]] = _get_urls_view_mapping('', url_pattern)
 
     return url_view_mapping
